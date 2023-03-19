1b8f9ab6dab18a7f08e2841a08369687575739ab
Ramveer99 <81071028+Ramveer99@users.noreply.github.com>
2023-03-19 10:49:54 +0530

    Added solution - LeetHub

--- Sort The Array - GFG/sort-the-array.java
+++ Sort The Array - GFG/sort-the-array.java
@@ -36,8 +36,7 @@ class Solution
     int[] sortArr(int[] arr, int n) 
     { 
         // code here
-        Arrays.sort(arr);
-        
-        return arr;
+       Arrays.sort(arr);
+       return arr;
     }
 } 
